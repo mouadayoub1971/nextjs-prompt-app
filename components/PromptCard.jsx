10ad0f30bf45f2463112e15b062c83be2fd9acfc
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2024-10-31 23:44:05 +0100

    l'ajout de MyProfile

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -2,24 +2,39 @@
 
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
+import { useState } from 'react';
+
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
- const { data: session } = useSession();
+  const { data: session } = useSession();
+  const [copied, setCopied] = useState("");
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000);
+  }
  console.log(post);
  return (
   <div className="prompt_card">
-   <div className="">
+   <div className="flex justify-between items-start gap-5">
     <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-     <Image src={post.creator.image} width={30} height={30} className=" rounded-full object-contain" />
+     <Image src={post.creator.image} alt="user_image" width={30} height={30} className=" rounded-full object-contain" />
      <div className="flex flex-col">
       <h3 className=" font-satoshi text-gray-900 font-semibold">{post.creator.username}</h3>
       <p className=" font-inter text-gray-500 text-sm">{post.creator.email}</p>
-      </div>
-         <div className='copy_btn' onClick={handleClick}>
-           <Image />
-      </div>
-    </div> 
+     </div>
+    </div>
+    <div className="copy_btn" onClick={handleCopy}>
+     <Image
+      src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'}
+      alt={copied == post.prompt ? 'thick_icon ' : 'copy_icon'}
+      width={20}
+      height={20}
+     />
+    </div>
    </div>
+   <p className="my-4 font-satoshi text-sm text-gray-700" onClick={()=>handleTagClick && handleTagClick(post.tag)}>{post.prompt}</p>
+   <p className="font-inter text-sm blue_gradient cursor-pointer"> {post.tag}</p>
   </div>
  );
 };
