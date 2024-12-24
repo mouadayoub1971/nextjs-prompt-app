2e7ee36f4dc82180472f6f81bf572f5046a31489
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2024-12-24 16:56:56 +0100

    fixing deployement errors

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState , useEffect} from 'react';
-import { useRouter , useSearchParams} from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 import Form from '@components/Form';
 
 const EditPrompt = () => {
@@ -47,6 +48,6 @@ const EditPrompt = () => {
    setSubmitting(false);
   }
  };
- return <Form type="Edit" post={post} setPost={setPost} submitting={submitting} handleSubmit={updatePrompt} />;
+ return<Suspense><Form type="Edit" post={post} setPost={setPost} submitting={submitting} handleSubmit={updatePrompt} /></Suspense> ;
 };
 export default EditPrompt;
